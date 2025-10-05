5fc26cb35082c450c13ecb7db723ba4eb61957ce
ceh6514 <ceh6514@mavs.uta.edu>
2025-10-05 02:51:54 -0500

    Support Auth0 build arguments

--- src/deploy.sh
+++ src/deploy.sh
@@ -5,9 +5,12 @@ REGION=us-south1
 SERVICE=meowtion-sensor
 
 gcloud config set project "$PROJECT"
+GEMINI_KEY=$(gcloud secrets versions access latest --secret=VITE_GEMINI_API_KEY)
+AUTH0_DOMAIN=$(gcloud secrets versions access latest --secret=VITE_AUTH0_DOMAIN)
+AUTH0_CLIENT_ID=$(gcloud secrets versions access latest --secret=VITE_AUTH0_CLIENT_ID)
 gcloud run deploy "$SERVICE" \
   --source . \
   --region "$REGION" \
   --allow-unauthenticated \
-  --set-build-env-vars VITE_GEMINI_API_KEY="$(gcloud secrets versions access latest --secret=VITE_GEMINI_API_KEY)"
+  --set-build-env-vars "VITE_GEMINI_API_KEY=${GEMINI_KEY},VITE_AUTH0_DOMAIN=${AUTH0_DOMAIN},VITE_AUTH0_CLIENT_ID=${AUTH0_CLIENT_ID}"
 
