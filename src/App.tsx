19d5fea2c9451e969254f820687931ce0fad80f3
Christopher Nguyen <christophern3839@gmail.com>
2025-10-05 11:44:03 -0500

    Removing stray F

--- src/App.tsx
+++ src/App.tsx
@@ -433,7 +433,7 @@ const App: React.FC = () => {
                     className="w-full h-full object-cover"
                   />
                 </button>
-              ))}F
+              ))}
             </div>
           )}
 
