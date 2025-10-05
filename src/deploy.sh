e385f7aaaea7cc69b6a932f8dc24d278bc6ed209
ceh6514 <ceh6514@mavs.uta.edu>
2025-10-05 07:02:40 -0500

    Working off branch

--- src/deploy.sh
+++ src/deploy.sh
@@ -34,7 +34,5 @@ gcloud run deploy meowtion-sensor \
   --source src \
   --region us-south1 \
   --allow-unauthenticated \
-  --build-arg VITE_GEMINI_API_KEY="${VITE_GEMINI_API_KEY}" \
-  --build-arg VITE_AUTH0_DOMAIN="${VITE_AUTH0_DOMAIN}" \
-  --build-arg VITE_AUTH0_CLIENT_ID="${VITE_AUTH0_CLIENT_ID}" \
+  --build-env-vars VITE_GEMINI_API_KEY="${VITE_GEMINI_API_KEY}",VITE_AUTH0_DOMAIN="${VITE_AUTH0_DOMAIN}",VITE_AUTH0_CLIENT_ID="${VITE_AUTH0_CLIENT_ID}" \
   --set-env-vars="GEMINI_API_KEY=${VITE_GEMINI_API_KEY},VITE_AUTH0_DOMAIN=${VITE_AUTH0_DOMAIN},VITE_AUTH0_CLIENT_ID=${VITE_AUTH0_CLIENT_ID}"
